f66a0371ead4830d5285b717d430a881c0bdf4f8
jefftechhub <jefftechhub@gmail.com>
2024-03-27 04:49:20 -0700

    build add

--- src/Axios.js
+++ src/Axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000",
-  // baseURL: "http://18.216.143.242/",
+  // baseURL: "http://localhost:5000",
+  baseURL: "http://18.216.143.242/",
 });
